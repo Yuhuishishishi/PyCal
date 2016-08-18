443ea7999ab1608861b1bf93c040a9bbe1bd9158
Yuhui Shi <yuhuishi@umich.edu>
2016-08-18 15:23:24 -0400

    add backward observation method

--- PyCal/HolidayParser.py
+++ PyCal/HolidayParser.py
@@ -1,4 +1,5 @@
 import datetime
+import calendar
 
 
 class Holiday:
@@ -39,6 +40,9 @@ class FloatingHoliday(Holiday):
         self.month = month
 
     def observe_in_year(self, year):
+        if self.ordinal < 0:
+            return self._observe_in_year_backward(year)
+
         ordinal = self.ordinal
         # get the first day in the month
         first_day_in_month = datetime.date(day=1, month=self.month, year=year)
@@ -52,6 +56,21 @@ class FloatingHoliday(Holiday):
 
         # plus the ordinal
         candidate = first_desired_weekday_in_month + datetime.timedelta(weeks=max(0, ordinal-1))
+        return candidate
+
+    def _observe_in_year_backward(self, year):
+        ordinal = self.ordinal
+        # assume ordinal is negative
+        ,num_days_in_month = calendar.monthrange(year, self.month)
+        last_day_in_month = datetime.date(year, self.month, num_days_in_month)
+        if last_day_in_month.weekday() >= self.weekday:
+            last_desired_weekday_in_month = last_day_in_month - datetime.timedelta(days=last_day_in_month.weekday()-self.weekday)
+        else:
+            # deviate to the previous week
+            last_desired_weekday_in_month = last_day_in_month + datetime.timedelta(weeks=-1, days=self.weekday - last_day_in_month.weekday())
+            ordinal += 1
+
+        candidate = last_desired_weekday_in_month + datetime.timedelta(weeks=min(0, ordinal+1))
         return candidate
 
 
